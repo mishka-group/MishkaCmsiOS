ba5b1e90eacd1d66a6291502092c2b1ec5b6450b
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2021-11-27 17:32:10 +0330

    Recreate and Rename project and some settings

---
+++ MishkaCmsiOS/MishkaCmsiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MishkaCmsiOSApp.swift
+//  MishkaCmsiOS
+//
+//  Created by shahryar on 9/6/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct MishkaCmsiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
