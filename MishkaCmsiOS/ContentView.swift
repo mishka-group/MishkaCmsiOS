78cb3879ce364a3192a41fee11d7d999a99cc8f0
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2021-11-27 17:41:13 +0330

    Start iOS project

---
+++ MishkaCmsiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MishkaCmsiOS
+//
+//  Created by shahryar on 9/6/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
